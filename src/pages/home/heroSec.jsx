174bfc08821b59be3b682f10aad12a240ddafa2f
Chamath Kalhara <ckalhara360@gmail.com>
2025-08-01 12:52:50 +0530

    adgest main img

--- src/pages/home/heroSec.jsx
+++ src/pages/home/heroSec.jsx
@@ -14,10 +14,10 @@ export default function Hero() {
     return (
         <div className="flex flex-col justify-center items-center relative ">
             <Header />
-            <div className="main relative w-[95vw] h-[50vh] flex justify-center rounded-[20px] bg-[#0B080A] position-relative flex-col items-center">
-                <div className='img-div rounded-[20px] flex items-start justify-between'>
-                    <img src="../public/Artboard 1.png" alt="img 1" srcset="" className="w-[43vw] justify-center relative left-[18vw] top-[-15vh] z-10" />
-                    <div className='  main-title p-[20px] w-[40vw] h-[54.5vh] flex-col justify-center text-start  text-[#C7C7C7] absolute top-[5vh] pl-30 items-start  '>
+            <div className="main relative w-[95vw] h-[50vh] flex justify-center rounded-[20px] bg-[#0B080A] position-relative flex-col items-center ">
+                <div className='img-div rounded-[20px] flex items-center justify-center '>
+                    <img src="../public/Artboard 002.png" alt="img 1" srcset="" className="w-[22vw]  z-10  justify-center relative top-10 " />
+                    <div className='  main-title p-[20px] w-[40vw] h-[54.5vh] flex-col justify-center text-start  text-[#C7C7C7]  pl-30 items-start absolute left-10 top-10 z-20'>
                         <h1 style={{fontSize:'13vh'}} >M I N D</h1> 
                         <h1>S C A P</h1>
                     </div>
